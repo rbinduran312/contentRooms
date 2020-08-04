a1d979a787e348fb040ee55e00f18b8d2efa78cc
rbinduran312 <rbinduran312@outlook.com>
2020-08-04 06:26:20 -0400

    environment

--- server.js
+++ server.js
@@ -39,15 +39,15 @@ app.use(function (err, req, res, next) {
 
 // Serve static assets in production
 console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+// }
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
