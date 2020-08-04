83cfcf70e1a19fdce2beee5e6aac30eb9c193eb0
rbinduran312 <rbinduran312@outlook.com>
2020-08-04 05:47:25 -0400

    cd - production.yml

---
+++ client/build/precache-manifest.2bc276c701f07fedbf36f6fc372d9ac9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd62e014d9143655f638debe7a831c47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e1a3e28ccd8fed37d51",
+    "url": "/static/css/2.d8678eda.chunk.css"
+  },
+  {
+    "revision": "c2422b852541560d346e",
+    "url": "/static/css/main.3ef89e74.chunk.css"
+  },
+  {
+    "revision": "1e1a3e28ccd8fed37d51",
+    "url": "/static/js/2.4f958a19.chunk.js"
+  },
+  {
+    "revision": "138eca5ea4ccf17f648e006df975d9b7",
+    "url": "/static/js/2.4f958a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2422b852541560d346e",
+    "url": "/static/js/main.fae8455a.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "8425f5abe76fd08caf8ed74c5d5a4a64",
+    "url": "/static/media/showcase.8425f5ab.jpg"
+  }
+]);
